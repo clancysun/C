219a266b2bd25d8e970dae830ebe6ced504fc712
One Zero <zeroonegit@gmail.com>
2015-11-03 20:56:19 +0800

    给hello_world.c文件添加上注释

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h>               // 包含标准库的信息
 
-main()
-{
-    printf("hello, world\n");
+main()                           // 定义名为main的函数，它不接受参数值
+{                                // main函数的语句都被括在花括号中
+    printf("hello, world\n");    // main函数调用库函数printf以显示字符序列；\n代表换行符
 }
