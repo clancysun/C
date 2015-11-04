886246294c55f8c121bec1a8c91be6c9c4f3125a
One Zero <zeroonegit@gmail.com>
2015-11-04 22:22:13 +0800

    编写一个打印EOF值的程序。

--- print_EOF.c
+++ print_EOF.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
 void main() {
-    printf("随便按个键，Ctrl+D或Ctrl+Z代表EOF\n");
-    printf("表达式getchar() != EOF 的值为 %d\n", getchar() != EOF);
+    printf("EOF的值用数字表示为：%d\n", EOF);
 }
