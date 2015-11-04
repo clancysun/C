d0199388359f075799769a53a059915e26ab5348
One Zero <zeroonegit@gmail.com>
2015-11-04 11:49:35 +0800

    把无意义的数字替换为有意义的符号常量

--- fahr_celsius_3.c
+++ fahr_celsius_3.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+#define     LOWER   0       /*  表的下限  */
+#define     UPPER   300     /*  表的下限  */
+#define     STEP    20      /*  表的下限  */
 /*  打印华氏温度-摄氏温度对照表  */
 void main(){
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
