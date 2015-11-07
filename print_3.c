9a9faa7b8f5ed5a5913cd20636058c23c4f86878
One Zero <zeroonegit@gmail.com>
2015-11-07 21:04:56 +0800

    多设几个字符数组分段存放含空格的串

---
+++ print_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*  多设几个字符数组分段存放含空格的串  */
+
+void main(){
+    char str1[20], str2[20], str3[20], str4[20];
+    printf("Input string: ");
+    scanf("%s %s %s %s", str1, str2, str3, str4);
+    printf("Your string: %s %s %s %s\n", str1, str2, str3, str4);
+}
