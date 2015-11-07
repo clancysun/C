3d53f377a3b67836d248080024fdba97d64856d2
One Zero <zeroonegit@gmail.com>
2015-11-07 21:11:27 +0800

    字符串长度函数strlen

---
+++ strlen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+/*  字符串长度函数strlen  */
+
+void main(){
+    char str[] = "C language";
+    int len = strlen(str);
+    printf("The lenth of the string is %d\n", len);
+}
