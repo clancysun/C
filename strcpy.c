75b8595a55ef72a6e58c51265e4f22f6e8408bc1
One Zero <zeroonegit@gmail.com>
2015-11-07 21:16:38 +0800

    字符串复制函数strcpy

---
+++ strcpy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+/*  字符串复制函数strcpy  */
+
+void main(){
+    char str1[15], str2[] = "C Language";
+    strcpy(str1, str2);
+    puts(str1);
+    printf("\n");
+}
