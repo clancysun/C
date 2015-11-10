b3ca61913f599a4cfd955e2736edd220c9318e5f
One Zero <zeroonegit@gmail.com>
2015-11-10 11:04:03 +0800

    输入 n，输出 200 / (n+1)^2 的值

--- sum_2.c
+++ sum_2.c
@@ -2,12 +2,12 @@
 
 /*  输入 n，输出 (n+1)^2 的值  */
 
-#define SQ(y) (y) * (y)
+#define SQ(y) ((y) * (y))
 
 void main(){
     int a, sq;
     printf("Input a number: ");
     scanf("%d", &a);
-    sq = SQ(a + 1);
+    sq = 200 / SQ(a + 1);
     printf("sq = %d\n", sq);
 }
