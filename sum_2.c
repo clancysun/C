a3890746325edb7a43450aea9a42a31b6723fecc
One Zero <zeroonegit@gmail.com>
2015-11-10 11:00:38 +0800

    输入 n，输出 (n+1)^2 的值

---
+++ sum_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*  输入 n，输出 (n+1)^2 的值  */
+
+#define SQ(y) (y) * (y)
+
+void main(){
+    int a, sq;
+    printf("Input a number: ");
+    scanf("%d", &a);
+    sq = SQ(a + 1);
+    printf("sq = %d\n", sq);
+}
